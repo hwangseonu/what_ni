fc692370ed679ede146c39c1647c0cd97b588332
hwangseonu <hwangseonu12@naver.com>
2019-02-05 15:20:30 +0900

    feat: Add user model

--- app/views/users/user.py
+++ app/views/users/user.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
 from mongoengine import Q
