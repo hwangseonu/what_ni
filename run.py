99253fe0840d516387636e4d542a52ebda6a8676
hwangseonu <hwangseonu12@naver.com>
2019-02-06 12:41:50 +0900

    fix: config

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import create_app
 from config.db_config import HerokuMongoDBConfig as MongoDBConfig
-from config.app_config import ProductionConfig
+from config.app_config import ProductionConfig as Config
 
 from constants import heroku_run as run
 
-app = create_app(MongoDBConfig, ProductionConfig)
+app = create_app(MongoDBConfig, Config)
 
 
 if __name__ == '__main__':
