98a7a837bb18e9cab33f483b2ad2399a78ac1674
hwangseonu <hwangseonu12@naver.com>
2019-02-06 12:33:16 +0900

    fix: heroku config

--- config/db_config.py
+++ config/db_config.py
@@ -1,6 +1,17 @@
+import os
+
+
 class LocalMongoDBConfig:
     MONGODB_SETTINGS = {
         'db': 'what_ni',
         'host': 'localhost',
+        'port': 27017
+    }
+
+
+class HerokuMongoDBConfig:
+    MONGODB_SETTINGS = {
+        'db': 'what_ni',
+        'host': os.getenv('MONGODB_URI'),
         'port': 27017
     }
--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from app import create_app
-from config.db_config import LocalMongoDBConfig
+from config.db_config import HerokuMongoDBConfig as MongoDBConfig
 from config.app_config import ProductionConfig
 
 from constants import heroku_run as run
 
-app = create_app(LocalMongoDBConfig, ProductionConfig)
+app = create_app(MongoDBConfig, ProductionConfig)
 
 
 if __name__ == '__main__':
